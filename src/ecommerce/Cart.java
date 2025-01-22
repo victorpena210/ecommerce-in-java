7059fbb93c72e39d6cf31797a94f2e48128f5479
Victor Pena <victor.javier.pena210@gmail.com>
2025-01-22 17:48:57 -0600

    created object Cart for ecommerce store with 2 nested classes discount & tax

--- src/ecommerce/Cart.java
+++ src/ecommerce/Cart.java
@@ -22,13 +22,104 @@ public class Cart {
 	
 	
 	
-	
-
 	public Cart() {
 		// TODO Auto-generated constructor stub
 	}
 	
 	
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public int getUserId() {
+		return userId;
+	}
+
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+
+	public BigDecimal getTotalNetPrice() {
+		return totalNetPrice;
+	}
+
+
+	public void setTotalNetPrice(BigDecimal totalNetPrice) {
+		this.totalNetPrice = totalNetPrice;
+	}
+
+
+	public BigDecimal getTotalGrossPrice() {
+		return totalGrossPrice;
+	}
+
+
+	public void setTotalGrossPrice(BigDecimal totalGrossPrice) {
+		this.totalGrossPrice = totalGrossPrice;
+	}
+
+
+	public BigDecimal getTotalTax() {
+		return totalTax;
+	}
+
+
+	public void setTotalTax(BigDecimal totalTax) {
+		this.totalTax = totalTax;
+	}
+
+
+	public Tax getTax() {
+		return tax;
+	}
+
+
+	public void setTax(Tax tax) {
+		this.tax = tax;
+	}
+	
+	public class Discount {
+		private String discountName;
+		private double discountRate;
+		
+
+		public Discount(String discountName, double discountRate) {
+			this.setDiscountName(discountName);
+			this.setDiscountRate(discountRate);
+		}
+
+
+		public String getDiscountName() {
+			return discountName;
+		}
+
+
+		public void setDiscountName(String discountName) {
+			this.discountName = discountName;
+		}
+
+
+		public double getDiscountRate() {
+			return discountRate;
+		}
+
+
+		public void setDiscountRate(double discountRate) {
+			this.discountRate = discountRate;
+		}
+		
+		
+	}
+
+
 	public static class Tax{
 		private String taxType;
 		private double taxRate;
